3b86e01024bb33c4385be2d44cf9a16da860adf2
Kellen Rosburg <krosburg@eng.ooirsn.uw.edu>
2018-11-14 23:36:18 +0000

    Totally redesigned the plotEng_Nav

--- plotEng_navList.py
+++ plotEng_navList.py
@@ -1,19 +1,14 @@
 from ooi_web import writeNavHeader, writeEngNavNodeLI, writeNavFooter
 from ooi_web import writeENGSummary
-try:
-    import cPickle as pickle
-except ModuleNotFoundError:
-    import pickle
+
+node_list = ['LJ01A', 'LJ01B', 'LJ01C', 'LJ01D', 'LJ03A', 'LV01A', 'LV01B',
+             'LV01C', 'LV03A', 'MJ01A', 'MJ01B', 'MJ01C', 'MJ03A', 'MJ03B', 
+             'MJ03C', 'MJ03D', 'MJ03E', 'MJ03F', 'PC01A', 'PC01B', 'PC03A',
+             'PD01A', 'PD01B', 'PD03A', 'SF01A', 'SF01B', 'SF03A']
 
 
 # Define Variables
 web_file = '/var/www/html/engm2m/navigationFile.html'
-eng_file = 'rsn_eng_streams.pkl'
-
-# Load RSN Data Structure
-print('Loading %s' % eng_file)
-with open(eng_file, 'rb') as input:
-    rsn = pickle.load(input)
 
 # Print HTML Header
 writeNavHeader(web_file, 'M2M Engineering Page')
@@ -22,21 +17,9 @@ writeNavHeader(web_file, 'M2M Engineering Page')
 writeENGSummary(web_file)
 
 # Loop on Sites, Nodes, Instruments, Streams
-for site in rsn.sites:
-    if not site.nodes:
-        continue
+for node in node_list:
 
-    # Loop on Nodes for Site
-    for node in site.nodes:
-        if not node.instruments:
-            continue
-
-        # Filter Instruments to IP Only
-#        node.filtIPOnly()
-        if not node.instruments:
-            continue
-
-        writeEngNavNodeLI(web_file, node.id)
+    writeEngNavNodeLI(web_file, node)
 
 # Finalize HTML FILE
 writeNavFooter(web_file)
